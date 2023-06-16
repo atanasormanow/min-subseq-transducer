c859468821265477aa2ccf566e2b5663da73cd0a
Atanas Ormanov <atanasormanow@gmail.com>
2023-06-16 14:11:43 +0300

    feat: create trivial transducer for 1 dict entry

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,5 @@ mod transducer;
 use transducer::Transducer;
 
 fn main() {
-    // TODO: read function from (csv?) file
-    let t = Transducer::from_dictionary(vec![]);
+    todo!()
 }
--- src/transducer.rs
+++ src/transducer.rs
@@ -1,42 +1,55 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-type Sigma = Vec<char>;
-
-type M = usize;
-type Monoid = Vec<M>;
-
-type Q = usize;
-type F = Q;
-type States = Vec<Q>;
-
+// T :: (Sigma, Monoid, Q, Init, F, Delata, Lambda, Iota, Psi)
+// Sigma :: [Char]
+// Monoid :: Int
+// Q :: [Int]
+// Init :: Int
+// F :: [Int]
+// Delta :: (Q, Sigma) -> Q
+// Lambda :: (Q, Sigma) -> Monoid
+// Iota :: Monoid
+// Psi :: F -> Monoid
 pub struct Transducer {
-    input_alphabet: Sigma,
-    output_alphabet: Monoid,
-    states: States,
-    init_state: Q,
-    states_finality: Vec<bool>,
-    delta: HashMap<(Q, char), Q>,
-    lambda: HashMap<(Q, char), M>,
-    psi: HashMap<F, M>,
+    alphabet: HashSet<char>,
+    states_with_finality: Vec<bool>,
+    init_state: usize,
+    delta: HashMap<(usize, char), usize>,
+    lambda: HashMap<(usize, char), usize>,
+    iota: usize,
+    psi: HashMap<usize, usize>,
 }
 
 impl Transducer {
-    pub fn from_dictionary(dictionary: Vec<String>) -> Self {
-        for s in dictionary {
-            // TODO: construct transducer components
-            print!("{}", s);
+    pub fn from_word(word: Vec<char>, output: usize) -> Self {
+        let mut alphabet = HashSet::new();
+        let mut states_with_finality: Vec<bool> = Vec::with_capacity(10000000);
+        let mut delta = HashMap::new();
+        let mut lambda = HashMap::new();
+        let mut psi = HashMap::new();
+
+        for i in 0..word.len() {
+            alphabet.insert(word[i]);
+            states_with_finality.push(false);
+            delta.insert((i, word[i + 1]), i + 1);
+            lambda.insert((i, word[i + 1]), 0);
         }
 
-        // TODO: construct with result components
-        Self {
-            input_alphabet: vec![],
-            output_alphabet: vec![],
-            states: vec![],
+        states_with_finality[word.len() - 1] = true;
+        psi.insert(word.len() - 1, 0);
+
+        return Self {
+            alphabet,
+            states_with_finality,
             init_state: 0,
-            states_finality: vec![],
-            delta: HashMap::new(),
-            lambda: HashMap::new(),
-            psi: HashMap::new(),
-        }
+            delta,
+            lambda,
+            iota: output,
+            psi,
+        };
+    }
+
+    pub fn from_dictionary(dictionary: Vec<String>) -> Self {
+        todo!();
     }
 }
