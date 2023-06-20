67a9a1a4261280b908e493da2b7ae529a66be0ba
Atanas Ormanov <atanasormanow@gmail.com>
2023-06-20 12:04:17 +0300

    refactor: change used data structures

--- src/transducer.rs
+++ src/transducer.rs
@@ -1,4 +1,8 @@
-use std::{collections::{BTreeSet, HashMap, HashSet}, panic};
+use std::{
+    cmp::min,
+    collections::{BTreeSet, HashMap, HashSet},
+    panic,
+};
 
 mod tests;
 mod utils;
@@ -23,58 +27,115 @@ impl Entry {
 }
 
 pub struct Transducer {
-    alphabet: HashSet<char>,
-    // I use this to avoid shifting when removing states
-    states: BTreeSet<usize>,
-    // NOTE: If i need to iterate over the final states,
-    // i would have to iterate over all the states
-    finality: Vec<bool>,
-    init_state: usize,
-    delta: HashMap<usize, HashMap<char, usize>>,
-    lambda: HashMap<usize, HashMap<char, usize>>,
-    iota: usize,
-    psi: HashMap<usize, usize>,
-    min_except: Vec<char>,
-    trans_order_partitions: Vec<BTreeSet<usize>>,
+    pub alphabet: HashSet<char>,
+    pub states: BTreeSet<usize>,
+    pub finality: HashSet<usize>,
+    pub init_state: usize,
+    pub delta: HashMap<usize, HashMap<char, usize>>,
+    pub lambda: HashMap<usize, HashMap<char, usize>>,
+    pub iota: usize,
+    pub psi: HashMap<usize, usize>,
+    pub min_except: Vec<char>,
+    pub trans_order_partitions: Vec<BTreeSet<usize>>,
 }
 
 impl Transducer {
-    pub fn from_entry(entry: Entry) -> Self {
+    pub fn from_entry(word: &str, output: usize) -> Self {
+        let entry = Entry::new(word, output);
         let n = entry.word.len();
+
         let mut alphabet = HashSet::new();
-        let mut finality: Vec<bool> = Vec::with_capacity(10000000);
         let mut delta = HashMap::new();
         let mut lambda = HashMap::new();
-        let mut psi = HashMap::new();
 
         for i in 0..n {
             alphabet.insert(entry.word[i]);
-            finality.push(false);
 
-            let mut state_transition = HashMap::new();
-            state_transition.insert(entry.word[i], i + 1);
+            let state_transition = HashMap::from([(entry.word[i], i + 1)]);
             delta.insert(i, state_transition);
 
-            let mut state_output = HashMap::new();
-            state_output.insert(entry.word[i], 0);
+            let state_output = HashMap::from([(entry.word[i], 0)]);
             lambda.insert(i, state_output);
         }
 
-        finality.push(true);
-        psi.insert(n, 0);
-
         return Self {
             alphabet,
-            states: (0..n + 1).collect(),
-            finality,
+            states: (0..=n).collect(),
+            finality: HashSet::from([n]),
             init_state: 0,
             delta,
             lambda,
             iota: entry.output,
-            psi,
-            min_except: Vec::new(),
+            psi: HashMap::from([(n, 0)]),
+            min_except: entry.word,
             trans_order_partitions: vec![BTreeSet::from([n]), (0..n).collect()],
         };
+    }
+
+    pub fn add_entry_in_order(&mut self, word: &str, output: usize) {
+        let new_entry = Entry::new(word, output);
+        let t_w = self.state_sequence(&self.min_except);
+        let k = longest_common_prefix(&self.min_except, &new_entry.word).len();
+        let new_state = self.finality.len(); // NOTE: states are never removed from finality
+        let new_suffix_len = self.min_except.len() - k;
+
+        // Make the transducer min except in (last_entry ^ new_entry)
+        for _ in 0..new_suffix_len {
+            self.reduce_except_by_one();
+        }
+
+        // Add new states for the missing suffix
+        for i in 0..new_suffix_len {
+            self.states.insert(new_state + i);
+        }
+
+        self.finality.insert(new_state + new_suffix_len);
+
+        // Add a transition from the existing state
+        self.add_delta_transition(k, new_entry.word[0], new_state + k + 1);
+        // Add the transitions for the missing suffix
+        for i in 0..new_suffix_len {
+            self.add_delta_transition(
+                new_state + i,
+                new_entry.word[new_entry.word.len() + i + 1],
+                new_state + i + 1,
+            );
+        }
+
+        self.iota = min(self.iota, new_entry.output);
+
+        for t in &t_w {
+            self.lambda.remove(t);
+        }
+        for i in 1..k {
+            let curr_output = self.lambda_i(i, new_entry.output);
+            let prev_output = self.lambda_i(i - 1, new_entry.output);
+            self.add_lambda_transition(t_w[i], self.min_except[i], curr_output - prev_output);
+        }
+        self.add_lambda_transition(
+            t_w[t_w.len() - 1],
+            new_entry.word[0],
+            new_entry.output - self.lambda_i(k - 1, new_entry.output),
+        ); // TODO: more lambda updates
+
+        // The resulting Transducer is minimal except in the new_entry
+        self.min_except = new_entry.word;
+    }
+
+    pub fn from_dictionary(dictionary: Vec<(&str, usize)>) -> Self {
+        if dictionary.is_empty() {
+            panic!("Cannot construct empty transducer (yet)!");
+        }
+
+        let (w, o) = dictionary[0];
+        let mut transducer = Transducer::from_entry(w, o);
+
+        for e in &dictionary[1..] {
+            let (w, o) = *e;
+            transducer.add_entry_in_order(w, o);
+        }
+
+        return transducer;
     }
 
     pub fn print(&self) {
@@ -87,20 +148,16 @@ impl Transducer {
         println!("T iota: {:?}", self.iota);
         println!("T psi: {:?}", self.psi);
         println!("T min_except: {:?}", self.min_except);
-        println!("T partitions by transition order: {:?}", self.trans_order_partitions);
+        println!(
+            "T partitions by transition order: {:?}",
+            self.trans_order_partitions
+        );
     }
 
-    // Make the transducer min except in (last_word ^ new_word)
-    fn reduce_except_prefix(&mut self, next_word: &Vec<char>) {
-        let common_prefix = longest_common_prefix(&self.min_except, next_word);
-        let reduction_length = self.min_except.len() - common_prefix.len();
-
-        for _ in 0..reduction_length {
-            self.reduce_except_step();
-        }
-    }
-
-    fn reduce_except_step(&mut self) {
+    ///////////////////
+    // Private functions:
+    //////////////////////
+    fn reduce_except_by_one(&mut self) {
         let w = &self.min_except;
         let t_w = self.state_sequence(&w);
 
@@ -111,7 +168,7 @@ impl Transducer {
         match self.state_eq(tn, &t_w) {
             Some(q) => {
                 self.states.remove(&tn);
-                self.finality[tn] = false;
+                self.finality.insert(tn);
 
                 // TODO: do this in a better way
                 let _ = self.delta.get(&tn).is_some_and(|tn_trans| {
@@ -132,32 +189,33 @@ impl Transducer {
         }
     }
 
-    // TODO: How to check for equal states?
+    // Check for equal states by:
     // 1) have states partitioned based on their number of transitions
     // 2) check if tn is equal to some state with the same number of transitions
     // TODO: test, optimise, refactor and test again
-    fn state_eq(&self, state: usize, t_w: &Vec<usize>) -> Option<usize> {
-        let state_trans_part = match self.finality[state] {
-            true => 0,
-            // Will panic if there is no transition with `state`
-            false => self.delta[&state].len(),
-        };
+    pub fn state_eq(&self, state: usize, t_w: &Vec<usize>) -> Option<usize> {
+        let state_is_final = self.finality.contains(&state);
+        // No transitions if delta(state) is undefined
+        let state_trans_part = self.delta.get(&state).map_or(0, |trans| trans.len());
 
         for q in &self.trans_order_partitions[state_trans_part] {
             if t_w.contains(q) {
                 continue;
             }
 
-            let cond1 = self.finality[state] == self.finality[*q];
-            let cond2 = !self.finality[state] || self.psi[&state] == self.psi[&q];
+            let cond1 = state_is_final == self.finality.contains(q);
+            let cond2 = !state_is_final || self.psi[&state] == self.psi[q];
             let mut cond3 = true;
 
             for a in &self.alphabet {
+                println!("Here i am in the for loop for: {:?}", self.alphabet);
                 let dsa = self.delta.get(&state)?.get(a);
                 let dqa = self.delta.get(&q)?.get(a);
                 let lsa = self.lambda.get(&state)?.get(a);
                 let lqa = self.lambda.get(&q)?.get(a);
 
+                println!("delta(state,a), lambda(state,a): {:?}, {:?}", dsa, lsa);
+                println!("delta(q,a), lambda(q,a): {:?}, {:?}", dqa, lqa);
                 match (dsa, dqa, lsa, lqa) {
                     (None, None, _, _) => (),
                     (Some(q1), Some(q2), Some(m1), Some(m2)) => {
@@ -167,6 +225,7 @@ impl Transducer {
                 }
             }
 
+            // Return the first match
             if cond1 && cond2 && cond3 {
                 return Some(*q);
             }
@@ -175,37 +234,60 @@ impl Transducer {
         return None;
     }
 
-    // delta[(q,a)] will panic if delta is not defined
-    // TODO: make private
+    // NOTE: delta[(q,a)] will panic if delta is not defined
     fn state_sequence(&self, w: &Vec<char>) -> Vec<usize> {
         let mut next = self.init_state;
-        let mut states = vec![next];
+        let mut path = vec![next];
 
         for i in 0..w.len() {
             next = self.delta[&next][&w[i]];
-            states.push(next);
+            path.push(next);
         }
 
-        return states;
+        return path;
     }
 
-    pub fn add_entry_in_order(&self, entry_raw: (&str, usize)) {
-        let entry = Entry::from_tuple(entry_raw);
-        todo!()
+    fn add_delta_transition(&mut self, q1: usize, a: char, q2: usize) {
+        match self.delta.get_mut(&q1) {
+            Some(dq1) => {
+                dq1.insert(a, q2);
+            }
+            None => {
+                let q1_trans = HashMap::from([(a, q2)]);
+                self.delta.insert(q1, q1_trans);
+            }
+        }
     }
 
-    pub fn from_dictionary(dictionary: Vec<(&str, usize)>) -> Self {
-        if dictionary.is_empty() {
-            panic!("Cannot construct empty transducer (yet)!");
+    fn add_lambda_transition(&mut self, q1: usize, a: char, m: usize) {
+        match self.lambda.get_mut(&q1) {
+            Some(dq1) => {
+                dq1.insert(a, m);
+            }
+            None => {
+                let q1_trans = HashMap::from([(a, m)]);
+                self.lambda.insert(q1, q1_trans);
+            }
+        }
+    }
+
+    // NOTE: make sure using min_except is enough
+    fn lambda_i(&self, i: usize, beta: usize) -> usize {
+        let word_prefix_i = &self.min_except[..=i].to_vec();
+        return min(self.iota + self.lambda_star(word_prefix_i), beta);
+    }
+
+    // TODO: seems like this has taken a word that is not in the language
+    // accumulate word output, starting from the initial state
+    fn lambda_star(&self, word: &Vec<char>) -> usize {
+        let mut output = 0;
+        let mut state = self.init_state;
+
+        for ch in word {
+            output += self.lambda[&state][ch];
+            state = self.delta[&state][ch];
         }
 
-        let first_entry = Entry::from_tuple(dictionary[0]);
-        let transducer = Transducer::from_entry(first_entry);
-
-        for e in &dictionary[1..] {
-            transducer.add_entry_in_order(*e);
-        }
-
-        return transducer;
+        return output;
     }
 }
